3f0f98aefc3586dabe59e0603984249ec6593226
gintu tom <gintutom@gmail.com>
2020-05-19 03:01:34 +0530

    add eventlisteners

--- src/components/join/join.js
+++ src/components/join/join.js
@@ -1,7 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import io from "socket.io-client";
+
+const socket = io("https://7xy2d.sse.codesandbox.io/");
 
 export default class Join extends React.Component {
+  state = {
+    name: "",
+    room: ""
+  };
+  componentDidMount() {
+    socket.on("news", data => {
+      console.log(data);
+      socket.emit("my other event", { my: "data" });
+    });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return <div>join</div>;
   }
